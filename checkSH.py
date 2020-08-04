7d2c228d267129d9d279356488899c0061c05a97
Akylzhan Sauranbay <akyl.sauranbai@gmail.com>
2020-08-04 13:02:14 +0600

    test

--- checkSH.py
+++ checkSH.py
@@ -1,4 +1,6 @@
 import os
 
-
-print(os.popen("sh checkSH.sh CSCI+151 | gunzip -c").read())
+if __name__ == "__main__":
+  while True:
+    print(os.popen("sh checkSH.sh CSCI+151").read())
+    print()
